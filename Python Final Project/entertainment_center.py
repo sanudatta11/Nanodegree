3e8d3bb6225bf99523f675b0c75df3b7d089648f
Rowhit Swami <rowhitswami1@gmail.com>
2017-06-24 22:15:06 +0530

    Final Python Project

---
+++ Python Final Project/entertainment_center.py
@@ -0,0 +1,33 @@
+import media
+import fresh_tomatoes
+
+tubelight = media.Movie("Tubelight",
+             "https://upload.wikimedia.org/wikipedia/en/0/04/Tubelight_Poster.jpg",
+             "https://www.youtube.com/watch?v=PGQRNKHJwH4&vl=en")
+bahubali = media.Movie("Bahubali-2",
+            "https://i1.wp.com/www.mazale.in/wp-content/uploads/2017/04/baahubali-2-2.jpg",
+            "https://www.youtube.com/watch?v=qD-6d8Wo3do")
+raees = media.Movie("Raees",
+         "http://www.bollywoodlife.com/wp-content/uploads/2016/08/16-Raees-srk-poster.jpg",
+         "https://www.youtube.com/watch?v=J7_1MU3gDk0")
+phillauri = media.Movie("Phillauri",
+             "http://static.koimoi.com/wp-content/new-galleries/2017/02/phillauri-movie-1.jpg",
+             "https://www.youtube.com/watch?v=uCTr7MGFK0U")
+ADHM = media.Movie("Ae Dil Hai Mushkil",
+        "http://www.filmyfolks.com/movie/images/ae-dil-hai-mushkil-movie-3.jpg",
+        "https://www.youtube.com/watch?gl=IN&hl=sk&v=Z_PODraXg4E&itct=CAoQpDAYBSITCOL204TwwM8CFQ1fTgodQswIazIHcmVsYXRlZEjvjOH0zrev0Hc%3D&client=mv-google-%27N")
+YJHD = media.Movie("Yeh Jawani Hai Diwani",
+        "http://www.cinehindi.com/wp-content/uploads/2015/08/fsada.png",
+        "https://www.youtube.com/watch?v=Rbp2XUSeUNE")
+rockstar = media.Movie("Rockstar",
+            "http://www.watch-bollywood-movies.com/wp-content/uploads/2011/12/Rockstar-2011-download-mp3-songs.jpg",
+            "https://www.youtube.com/watch?v=rHsesDDDaa4")
+barfi = media.Movie("Barfi",
+         "https://upload.wikimedia.org/wikipedia/en/2/2e/Barfi%21_poster.jpg",
+         "https://www.youtube.com/watch?v=nQ3FYUgSjC8")
+tamasha = media.Movie("Tamasha",
+           "https://cdn.pinkvilla.com/files/styles/contentpreview/public/CPPUFYYUYAA9Snp.jpg?itok=knLTFxRR",
+           "https://www.youtube.com/watch?v=o-e5eWVCzx8")
+
+movies = [tubelight, bahubali, raees, phillauri, ADHM, YJHD, rockstar, barfi, tamasha]
+fresh_tomatoes.open_movies_page(movies)
